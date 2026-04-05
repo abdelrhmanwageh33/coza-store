ae578e09fa43318a0df2e6e23a1e6f1a24e4d653
abdelrhmanwageh33 <abdelrhmanwageh413@gmail.com>
2026-04-05 08:50:07 +0200

    change middlewar

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,20 @@
-// import { getToken } from 'next-auth/jwt'
-// import { NextResponse, NextRequest } from 'next/server'
- 
-// // This function can be marked `async` if using `await` inside
-// export async function middleware(request: NextRequest) {
-//  const token=  await getToken({req:request})
-//  if(token){
-// return NextResponse.next()
-//  }else{
-//   return NextResponse.redirect(new URL('/Login', request.url))
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-//  }
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
 
+  if (token) {
+    return NextResponse.next();
+  }
 
-// }
- 
-// export const config = {
-//   matcher: '/cart',
-// }
+  return NextResponse.redirect(new URL("/Login", request.url));
+}
+
+export const config = {
+  matcher: ["/cart/:path*"],
+};
